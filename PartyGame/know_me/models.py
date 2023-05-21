ae53c577492b9285ba17dc948b54bd97f07e53db
Shishkin Denis <mojento3@yandex.ru>
2023-05-21 22:10:52 +0500

    add model

--- PartyGame/know_me/models.py
+++ PartyGame/know_me/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+
+    question = models.CharField(max_length=300, name="вопрос")
+    # answer = models.CharField("ответ", max_length=300)
+
+    # def __str__(self):
+    #     return str(self.question)
+
+
+
--- PartyGame/know_me/views.py
+++ PartyGame/know_me/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def test(request):
+    print("request:", request.GET)
+    return HttpResponse(f"<h1>Страница в разработке</h1>"
+                        f"requst = {request.GET}"
+                        # f"requst = {request}"
+                        )
+
+def get_question(request):
+
+    con = {
+        "title": "question title"
+    }
+    return render(request, "know_me/question.html", con)
+
