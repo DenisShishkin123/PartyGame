d61bb3305cdcde2322909a9dacf5b3494e40fb6c
Shishkin Denis <mojento3@yandex.ru>
2023-05-21 20:38:55 +0500

    init

---
+++ 0 DOC/0 TODO.py
@@ -0,0 +1,23 @@
+
+
+# PartyGame
+# know_me
+# truth or action
+# code names
+"""
+
+"""
+
+# know_me
+# TODO git
+# TODO model
+# TODO view
+# TODO html
+# TODO
+
+# TODO html - home and menu
+
+
+
+
+
